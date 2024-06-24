c8aa5965fa7cf63d2794e69d820dd9995731cdd1
praiseee <S10255495@connect.np.edu.sg>
2024-06-24 12:49:16 +0800

    Gun system, Pick and Drop
    Ensure gun can be picked up and dropped at any point of time in game. Ensure gun is consistent with movement on camera

--- Assets/Scripts/PickUpController.cs
+++ Assets/Scripts/PickUpController.cs
@@ -7,6 +7,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Polybrush;
 
 public class PickUpController : MonoBehaviour
 {
@@ -21,6 +22,27 @@ public class PickUpController : MonoBehaviour
     public bool equipped;
     public static bool slotFull;
 
+    private void Start()
+    {
+        /// <summary>
+        /// SetUp
+        /// </summary>
+        
+        if (!equipped)
+        {
+            gunScript.enabled = false;
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+        }
+
+        if (equipped)
+        {
+            gunScript.enabled = true;
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+        }
+    }
+
     private void Update()
     {
         /// <summary>
@@ -33,6 +55,14 @@ public class PickUpController : MonoBehaviour
     private void PickUp()
     {
         equipped = true;
+
+        /// <summary>
+        /// Make weapon a child of the camera and move it to default position
+        /// </summary>
+        transform.SetParent(gunContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
 
         /// <summary>
         /// Make RigidBody kinematic and BoxCollider to trigger
@@ -51,10 +81,21 @@ public class PickUpController : MonoBehaviour
         equipped = false;
 
         /// <summary>
-        /// Make RigidBody NOT kinematic and BoxCollider to trigger
+        /// Set parent to null
+        /// </summary>
+        transform.SetParent(null);
+
+
+        /// <summary>
+        /// Make RigidBody NOT kinematic and BoxCollider to normal
         /// </summary>
         rb.isKinematic = false;
         coll.isTrigger = false;
+
+        /// <summary>
+        /// Gun carries momentum of player
+        /// </summary>
+        rb.velocity = player.GetComponent<Rigidbody>().velocity;
 
         /// <summary>
         /// Disable Script
