d19353d5b01d5c006320059d84a83c462166a635
praiseee <S10255495@connect.np.edu.sg>
2024-06-24 22:46:50 +0800

    AI Enemy Draft 2
    Enemy follows player successfully.
    Bug: Flying in the air.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,18 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    /// <summary>
+    /// Patrolling
+    /// </summary>
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    /// <summary>
+    /// Attacking
+    /// </summary>
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+    /// <summary>
+    /// States
+    /// </summary>
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    public void Awake()
     {
-        
+        player = GameObject.Find("PlayerCapsule").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        /// <summary>
+        /// Check for player in sight and attack at range
+        /// </summary>
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (!playerInAttackRange && !playerInSightRange) AttackPlayer();
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        /// <summary>
+        /// Walkpoint Reached
+        /// </summary>
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+
+    }
+
+    private void SearchWalkPoint()
+    {
+        /// <summary>
+        /// Calculate random point range
+        /// </summary>
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3 (transform.position.x + randomX, transform.position.y, transform.position.z +randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        /// <summary>
+        /// Make sure enemy does not move
+        /// </summary>
+        agent.SetDestination(transform.position);
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            /// <summary>
+            /// Attack Code starts here
+            /// </summary>
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+        }    
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
     }
 }
+
