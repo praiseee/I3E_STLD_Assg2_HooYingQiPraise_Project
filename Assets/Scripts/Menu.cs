aea28d3e5c6b99486ca3efd251457e71b0ec651e
praiseee <S10255495@connect.np.edu.sg>
2024-06-26 04:19:48 +0800

    Created Menu UI

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Load the game scene
+        SceneManager.LoadScene("Main");
+    }
+
+    public void SettingsOptions()
+    {
+        // Implement options menu logic here
+        Debug.Log("Options button clicked");
+    }
+
+    public void ShowCredits()
+    {
+        // Implement credits logic here
+        Debug.Log("Credits button clicked");
+    }
+
+    public void QuitGame()
+    {
+        // Quit the application
+        Application.Quit();
+    }
+}
