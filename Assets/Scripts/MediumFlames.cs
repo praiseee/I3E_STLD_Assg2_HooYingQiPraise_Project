fe53f1ff79567c1d06c7f0422777389623672574
praiseee <S10255495@connect.np.edu.sg>
2024-06-26 21:22:40 +0800

    Health Deduction in hazards

--- Assets/Scripts/MediumFlames.cs
+++ Assets/Scripts/MediumFlames.cs
@@ -1,7 +1,7 @@
 /*
  * Author: Hoo Ying Qi Praise
  * Date: 23/06/2024
- * Description: MediumFlames hazard script that reduces player health when touched.
+ * Description: MediumFlames hazard script that reduces player health when touched
  */
 
 using UnityEngine;
@@ -14,8 +14,8 @@ public class MediumFlames : MonoBehaviour
         Player player = other.GetComponent<Player>();
         if (player != null)
         {
-            // Reduce player's health by 2
-            player.TakeDamage(2);
+            // Reduce player's health
+            player.TakeDamage(4);
         }
     }
 }
