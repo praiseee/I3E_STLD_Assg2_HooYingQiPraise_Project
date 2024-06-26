93ceb1fe3cfb8f588db2f89cc6dc0a5768e1b9a6
praiseee <S10255495@connect.np.edu.sg>
2024-06-27 02:00:30 +0800

    Created Option Button for Wrong Answer
    Deals damage when players select the wrong asnwer

---
+++ Assets/Scripts/WrongAnswerButton.cs
@@ -0,0 +1,24 @@
+/*
+ * Author: Hoo Ying Qi Praise
+ * Date: 23/06/2024
+ * Description: Medkit script for handling medkit pickup and usage
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrongAnswerButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
