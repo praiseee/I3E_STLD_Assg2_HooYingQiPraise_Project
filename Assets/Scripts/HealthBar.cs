85bb2d9722b929a77de6ff56fd93dc981cb60708
praiseee <S10255495@connect.np.edu.sg>
2024-06-26 17:06:12 +0800

    Update comments on HealthBar Script

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -12,12 +12,21 @@ public class HealthBar : MonoBehaviour
     public Slider slider;
     public Image fill;
 
+    /// <summary>
+    /// Updates the current health value displayed on the health bar.
+    /// </summary>
+    /// <param name="health"></param>
     public void SetHealth(float health)
     {
+        //Updates the slider's value to the current health value, 
         slider.value = health;
         Debug.Log("Current health:" + slider.value);
     }
 
+    /// <summary>
+    ///  Sets the maximum health value and initializes the health bar to its maximum state
+    /// </summary>
+    /// <param name="max"></param>
     public void SetMaxHealth(float max)
     {
         slider.maxValue = max;
