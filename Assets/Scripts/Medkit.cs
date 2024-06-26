1e7630bd3ace1f93ef62549c4bb019663f1aaa25
praiseee <S10255495@connect.np.edu.sg>
2024-06-26 23:34:17 +0800

    Medkit Draft 1
    Medkit can be used to heal player. Heal can only happen once

--- Assets/Scripts/Medkit.cs
+++ Assets/Scripts/Medkit.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+/*
+ * Author: Hoo Ying Qi Praise
+ * Date: 23/06/2024
+ * Description: Medkit script for handling medkit pickup and usage
+ */
+
 using UnityEngine;
 
 public class Medkit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool isInRange = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isInRange && Input.GetButtonDown("Use"))
+        {
+            Player player = GameObject.FindWithTag("Player").GetComponent<Player>();
+            if (player != null)
+            {
+                player.AddMedkit();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,36 @@
 /*
  * Author: Hoo Ying Qi Praise
  * Date: 23/06/2024
- * Description: Player script handling health and damage
+ * Description: Player script handling health, damage, and medkits
  */
 
 using UnityEngine;
+
 public class Player : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
     public HealthBar playerHealthBar;
+    private int medkitCount = 0; // To track the number of medkits the player has
 
     void Start()
     {
         currentHealth = maxHealth;
         playerHealthBar.SetMaxHealth(maxHealth); // Initialize max health
         playerHealthBar.SetHealth(currentHealth); // Initialize current health display
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && medkitCount > 0)
+        {
+            UseMedkit();
+        }
     }
 
     public void TakeDamage(int damage)
@@ -32,5 +47,26 @@ public class Player : MonoBehaviour
     void Die()
     {
         GameManager.Instance.GameOver();
+    }
+
+    public void AddMedkit()
+    {
+        medkitCount++;
+        Debug.Log("Medkit picked up. Total medkits: " + medkitCount);
+    }
+
+    void UseMedkit()
+    {
+        if (medkitCount > 0)
+        {
+            currentHealth = Mathf.Min(currentHealth + 5, maxHealth); // Increase health but not above maxHealth
+            playerHealthBar.SetHealth(currentHealth); // Update current health display
+            medkitCount--;
+            Debug.Log("Used a medkit. Remaining medkits: " + medkitCount);
+        }
+        else
+        {
+            Debug.Log("No medkits left to use.");
+        }
     }
 }
